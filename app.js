4cf8c88ea835baa5796a16453b303a428b3f1268
g21472333 <g21472333@gmail.com>
2026-06-15 13:08:17 +0000

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const calculadora= require('./calculadora.js')
+
+let resultadosoma = calculadora.soma(10,20)
+console.log(resultadosoma)
+
+let resultadosubtracao = calculadora.subtracao(10,20)
+console.log(resultadosubtracao)
+
+let resultadodivisao = calculadora.divisao(10,300)
+console.log(resultadodivisao)
+
+let resultadomultiplicacao = calculadora.multiplicacao(100,20)
+console.log(resultadomultiplicacao)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ calculadora.test.js
@@ -0,0 +1,5 @@
+const calculadora= require('./calculadora.js')
+
+let resultado = calculadora.soma(10,20)
+console.log(resultado)
+clear
